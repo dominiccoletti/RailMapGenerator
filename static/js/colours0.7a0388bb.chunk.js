e95b314c2ce48e1a84ff9a363a65079da47e776c
Release Agent <rmg.release.agent@users.noreply.github.com>
2021-02-05 18:52:21 +0000

    Release RMG version  to PRD

--- static/js/colours0.7a0388bb.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonprail-map-generator"]=this["webpackJsonprail-map-generator"]||[]).push([[72],{108:function(a,e,p){a.exports=p.p+"static/media/README.5aa3f89e.md"}}]);
-//# sourceMappingURL=colours0.7a0388bb.chunk.js.map
